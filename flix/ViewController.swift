6dd940218973b61e0c4ccd51e88d525573b610a6
Michael Wornow <miking98@fb.com>
2017-06-21 16:05:36 -0700

    Scrolling images and overview on home screen

--- flix/ViewController.swift
+++ flix/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var photoMainTableView: UITableView!
+    
+    // Info on returned movies
+    var movies: [[String: Any]] = []
+    var totalMovies = 0
+    // The Movie Database URL's
+    let API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    let configURL = "https://api.themoviedb.org/3/configuration"
+    var baseImageURL = ""
+    let nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing"
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set up The Movie DB config information
+        let url = URL(string: generateURL(apiURL: configURL))!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data, let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                let images = dataDictionary["images"] as! [String: Any]
+                self.baseImageURL = images["secure_base_url"] as! String
+                
+                // Fetch data from The Movie DB
+                let url = URL(string: self.generateURL(apiURL: self.nowPlayingURL, page: 1))!
+                let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+                session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+                let task = session.dataTask(with: url) { (data, response, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else if let data = data, let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        self.movies = dataDictionary["results"] as! [[String: Any]]
+                        self.totalMovies = dataDictionary["total_results"] as! Int
+                    }
+                    self.photoMainTableView.reloadData()
+                }
+                task.resume()
+            }
+            self.photoMainTableView.reloadData()
+        }
+        task.resume()
+        
+        //Set up table view
+        photoMainTableView.delegate = self
+        photoMainTableView.dataSource = self
+    }
+    
+    func generateURL(apiURL: String, page: Int? = nil) -> String {
+        var url = apiURL + "?api_key=" + API_KEY
+        if page != nil {
+            url += "&page=" + String(page!)
+        }
+        return url
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = photoMainTableView.dequeueReusableCell(withIdentifier: "photoMain") as! photoMainTableViewCell
+        let movie = movies[indexPath.row]
+        
+        let title = movie["title"] as! String
+        let description = movie["overview"] as! String
+        let imageURL = movie["poster_path"] as! String
+        
+        cell.movieTitleLabel.text = title
+        cell.descriptionLabel.text = description
+        if baseImageURL != "" {
+            let fullImageURL = URL(string: baseImageURL + "w500" + imageURL)!
+            cell.photoImageView.af_setImage(withURL: fullImageURL)
+        }
+        
+        return cell
+    }
+    
 
 }
 
---
+++ flix/photoMainTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  photoMainTableViewCell.swift
+//  flix
+//
+//  Created by Michael Wornow on 6/21/17.
+//  Copyright © 2017 Michael Wornow. All rights reserved.
+//
+
+import UIKit
+
+class photoMainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
